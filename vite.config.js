b2f403b849c5b2320014ad3a47faf9ec69acf8ef
Muhammad Sabeel Yousaf <93435425+sabeelyousaf@users.noreply.github.com>
2025-06-25 14:19:56 +0500

    latest-code

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
-    port: 3040
+    port: 3042
   }
 });
